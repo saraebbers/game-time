535ef5834f1df80c7b05be5b6db5b10f01e3f9dc
Tim Garrity <tag.garrity@gmail.com>
2018-08-15 20:19:07 -0600

    make frog tests

--- test/frog-test.js
+++ test/frog-test.js
@@ -4,24 +4,59 @@ const Frog = require ('../lib/frog');
 describe ('Frog', () => {
   it('should start with three lives', () => {
     //setup
-    
+    const frog = new Frog(300, 550, 40, 40, 'green', 'pink');
     //execution
+
     //assertion
+    assert.equal(frog.lives, 3)
   })
   it.skip('after dying three times the game should be over', () => {
     //setup
+    const frog = new Frog(300, 550, 40, 40, 'green', 'pink');
     //execution
-    //assertion    
+    isDead();
+    isDead();
+    isDead();
+    //assertion
+    assert.isTrue(game.gameOver);
   })
   it.skip('it should move move the same direction as the arrow pressed', () => {
     //setup
-    //execution
-    //assertion
+    const frog = new Frog(300, 550, 40, 40, 'green', 'pink');
+    frogMoves(down);
+    assert.equal(frog.y, 551)
+
+    const frog = new Frog(300, 550, 40, 40, 'green', 'pink');
+    frogMoves(up);
+    assert.equal(frog.y, 549);
+
+    const frog = new Frog(300, 550, 40, 40, 'green', 'pink');
+    frogMoves(left);
+    assert.equal(frog.x, 299)
+
+    const frog = new Frog(300, 550, 40, 40, 'green', 'pink');
+    frogMoves(right);
+    assert.equal(frog.x, 301);
   })
-  it.skip('it should stop when it hits a wall', () => {
+  
+  it.skip('it should recognize when it hits a wall', () => {
     //setup
+    const frog = new Frog(0, 100, 40, 40, 'green', 'pink');
+
+    isCollidingWithWall();
+
+    assert.equal(isCollidingWithWall, true);
+
+    const frog = new Frog(600, 100, 40, 40, 'green', 'pink');
+
+    isCollidingWithWall();
+
+    assert.equal(isCollidingWithWall, true)
     //execution
     //assertion    
+  })
+  it.skip('it should not move further than the wall', () => {
+    const frog = new Frog (0, 100, 40, 40, 'green', 'pink')
   })
   it.skip('it should score y < 100 or the end of the game', () => {
     //setup
