ad3af2b7508fc89b60fba71f09dc1aa088373f3b
Sara Ebbers <johndoe@example.com>
2018-08-15 18:39:07 -0600

    Write Obstacle tests in human speak

---
+++ test/Obstacle-test.js
@@ -0,0 +1,36 @@
+const { assert } = require('chai');
+const Obstacle = require ('../lib/Obstacle');
+
+describe ('Obstacle', () => {
+  it('should instanciate new obstacles', () => {
+    //setup
+    
+    //execution
+    //assertion
+  })
+  it.skip('it should start on the 0 or 600 x axis', () => {
+    //setup
+    //execution
+    //assertion    
+  })
+  it.skip('it should move horizotally across the screen', () => {
+    //setup
+    //execution
+    //assertion
+  })
+  it.skip('it should disappear when it hits a wall', () => {
+    //setup
+    //execution
+    //assertion    
+  })
+  it.skip('it should console.log when colliding with frog', () => {
+    //setup
+    //execution
+    //assertion    
+  })
+  it.skip('it should appear to move smooth like cool jazz axcross the screen', () => {
+    //setup
+    //execution
+    //assertion    
+  })
+})
