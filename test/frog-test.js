b5583feb58131cdf5d7f6180fa1bed872df013ab
Tim Garrity <tag.garrity@gmail.com>
2018-08-15 18:14:50 -0600

    Write tests in human talk

---
+++ test/frog-test.js
@@ -0,0 +1,51 @@
+const { assert } = require('chai');
+const Frog = require ('../lib/frog');
+
+describe ('Frog', () => {
+  it('should start with three lives', () => {
+    //setup
+    
+    //execution
+    //assertion
+  })
+  it.skip('after dying three times the game should be over', () => {
+    //setup
+    //execution
+    //assertion    
+  })
+  it.skip('it should move move the same direction as the arrow pressed', () => {
+    //setup
+    //execution
+    //assertion
+  })
+  it.skip('it should stop when it hits a wall', () => {
+    //setup
+    //execution
+    //assertion    
+  })
+  it.skip('it should score y < 100 or the end of the game', () => {
+    //setup
+    //execution
+    //assertion    
+  })
+  it.skip('should die when hit by a vehicle', () => {
+    //setup
+    //execution
+    //assertion    
+  })
+  it.skip('it should die when not a log or turtle', () => {
+    //setup
+    //execution
+    //assertion    
+  })
+  it.skip('match the movement the of the log/turtle when colliding', () => {
+    //setup
+    //execution
+    //assertion    
+  })
+  it.skip('it should appear to jump', () => {
+    //setup
+    //execution
+    //assertion    
+  })
+})
