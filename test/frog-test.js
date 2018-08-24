075724eb8810b849a71434b1a5a7ef86064a88c4
Tim Garrity <tag.garrity@gmail.com>
2018-08-24 09:09:58 -0600

    make tests

--- test/frog-test.js
+++ test/frog-test.js
@@ -1,14 +1,69 @@
 const { assert } = require('chai');
 const Frog = require ('../lib/frog');
+const Game = require('../lib/Game');
+const Water = require('../lib/Water');
+const ctx = {
+  canvas: {
+    width: 600,
+    height: 600,
+  }
+};
+
 
 describe ('Frog', () => {
-  it('should start with three lives', () => {
+  it('should be an object', () => {
+    const frog = new Frog(300, 550, 40, 40, 'green', 'pink');
+    const newGame = new Game();
+
+    assert.isObject(newGame)
+    assert.isObject(frog);
+  });
+  it('should be a function', () => {
+    const frog = new Frog(300, 550, 40, 40, 'green', 'pink');
+    const newGame = new Game();
+
+
+    assert.isFunction(frog.isDead);
+    assert.isFunction(frog.isCollidingWith);
+    assert.isFunction(frog.frogMove);
+    assert.isFunction(frog.draw);
+
+  })
+  it('should move when a direction pressed',() => {
+    const frog = new Frog(300, 550, 40, 40, 'green', 'pink');
+    const newGame = new Game();
+
+    frog.frogMove(frog, 'right');
+
+    assert.equal(frog.x, 350);
+
+
+  })
+  it('should take properties', () => {
     //setup
     const frog = new Frog(300, 550, 40, 40, 'green', 'pink');
     //execution
 
     //assertion
-    assert.equal(frog.lives, 3)
+    assert.equal(frog.lives, 3);
+    assert.equal(frog.x, 300);
+    assert.equal(frog.y, 550);
+    assert.equal(frog.height, 40);
+    assert.equal(frog.width, 40);
+    assert.equal(frog.color, 'green')
+    assert.equal(frog.borderColor, 'pink')
+
+    // =========
+    // animate()
+    // move()
+    // animate()
+  })
+  it('should collide with walls', () => {
+    const frog = new Frog(300, 550, 40, 40, 'green', 'pink');
+    
+    frog.x = ctx.canvas.width;
+    assert.isTrue(frog.isCollidingWithWall(ctx.canvas.width, ctx.canvas.height));
+
   })
   it('after dying three times the game should be over', () => {
     //setup
